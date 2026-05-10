9c3e2929d980dac0766d30434ec6acc93efda678
olli_lo <lobigoliver@gmail.com>
2026-05-10 17:35:26 +0200

    *wip* workaround for flag resource

--- app/index.service.worker.js
+++ app/index.service.worker.js
@@ -4,7 +4,7 @@
 // Incrementing CACHE_VERSION will kick off the install event and force
 // previously cached resources to be updated from the network.
 /** @type {string} */
-const CACHE_VERSION = '1778426075|688383893';
+const CACHE_VERSION = '1778427161|135318393';
 /** @type {string} */
 const CACHE_PREFIX = 'BaguetteFrench-sw-cache-';
 const CACHE_NAME = CACHE_PREFIX + CACHE_VERSION;
