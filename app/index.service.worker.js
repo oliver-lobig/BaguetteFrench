301adf5695c87440a051e8dfbe353fd786070216
olli_lo <lobigoliver@gmail.com>
2026-05-14 12:51:22 +0200

    test 2 of v1.1.1

--- app/index.service.worker.js
+++ app/index.service.worker.js
@@ -4,7 +4,7 @@
 // Incrementing CACHE_VERSION will kick off the install event and force
 // previously cached resources to be updated from the network.
 /** @type {string} */
-const CACHE_VERSION = '1778754158|2218420580';
+const CACHE_VERSION = '1778755779|1002079662';
 /** @type {string} */
 const CACHE_PREFIX = 'BaguetteFrench-sw-cache-';
 const CACHE_NAME = CACHE_PREFIX + CACHE_VERSION;
