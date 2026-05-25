89f873e4a58995c5f29713ba87355fb5e5d42fea
olli_lo <lobigoliver@gmail.com>
2026-05-25 17:11:40 +0200

    *usable* activate IMPORTant fixes

--- app/index.service.worker.js
+++ app/index.service.worker.js
@@ -4,7 +4,7 @@
 // Incrementing CACHE_VERSION will kick off the install event and force
 // previously cached resources to be updated from the network.
 /** @type {string} */
-const CACHE_VERSION = '1779117075|51726824';
+const CACHE_VERSION = '1779721768|601539986';
 /** @type {string} */
 const CACHE_PREFIX = 'BaguetteFrench-sw-cache-';
 const CACHE_NAME = CACHE_PREFIX + CACHE_VERSION;
