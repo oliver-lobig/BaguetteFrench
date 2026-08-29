12c7912b8972ab3299b7dd8990d56e8c84abeb66
oliver-lobig <lobig.oliver@gmail.com>
2026-08-29 20:18:51 +0200

    chapter fix

--- app/index.service.worker.js
+++ app/index.service.worker.js
@@ -4,7 +4,7 @@
 // Incrementing CACHE_VERSION will kick off the install event and force
 // previously cached resources to be updated from the network.
 /** @type {string} */
-const CACHE_VERSION = '1779721768|601539986';
+const CACHE_VERSION = '1788026970|601932289';
 /** @type {string} */
 const CACHE_PREFIX = 'BaguetteFrench-sw-cache-';
 const CACHE_NAME = CACHE_PREFIX + CACHE_VERSION;
